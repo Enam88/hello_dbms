ee7bb0855ae26caf18574b38a9eaacb87da0248d
Enam AKLI <akli.ena88@gmail.com>
2024-01-05 14:43:04 +0100

    job_1_updated

--- job_1.sql
+++ job_1.sql
@@ -1,3 +1,10 @@
-SELECT Population 
+SELECT (Population) FROM world  
+WHERE Country = 'Germany' 
 
-FROM Countries.
+SELECT  Country, Population  
+FROM world 
+WHERE Country IN ('Germany' ,'Norway', 'Denmark') 
+
+SELECT Country, Area_sq_mi 
+FROM world 
+WHERE Area_sq_mi BETWEEN 200000 AND 300000 
